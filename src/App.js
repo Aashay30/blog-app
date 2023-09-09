b02750760d8fb22bea993f435883b6bb92b549f4
Aashay Tamrakar <tamrakaraashay30@gmail.com>
2023-09-09 12:35:55 +0530

    added-write-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavBar from "./components/nav-bar/NavBar";
-import Single from "./pages-of-blog/singlePage/Single";
+// import Single from "./pages-of-blog/singlePage/Single";
+import WritePage from "./pages-of-blog/writePage/WritePage";
 // import Home from "./pages-of-blog/home-page/Home";
 // import Single from "./pages-of-blog/singlePage/Single";
 
 function App() {
   return <div className="App">
     <NavBar />
-    <Single />
+    <WritePage />
   </div>;
 }
 
---
+++ src/pages-of-blog/writePage/WritePage.jsx
@@ -0,0 +1,38 @@
+import "./writepage.css";
+
+export default function WritePage() {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input id="fileInput" type="file" style={{ display: "none" }} />
+          <input
+            className="writeInput"
+            placeholder="Title"
+            type="text"
+            autoFocus={true} /* this make the text area auto selected so that when we type it gets filled in here */
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}  
+          />
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
+}
