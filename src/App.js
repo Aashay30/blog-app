2ca23dfa7c13a9a77ff02c6567a96d65b3864db5
Aashay Tamrakar <tamrakaraashay30@gmail.com>
2023-09-09 12:57:49 +0530

    settings-page-added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import NavBar from "./components/nav-bar/NavBar";
+import Settings from "./pages-of-blog/settingsPage/Settings";
 // import Single from "./pages-of-blog/singlePage/Single";
-import WritePage from "./pages-of-blog/writePage/WritePage";
+// import WritePage from "./pages-of-blog/writePage/WritePage";
 // import Home from "./pages-of-blog/home-page/Home";
 // import Single from "./pages-of-blog/singlePage/Single";
 
 function App() {
   return <div className="App">
     <NavBar />
-    <WritePage />
+    <Settings />
   </div>;
 }
 
---
+++ src/pages-of-blog/settingsPage/Settings.jsx
@@ -0,0 +1,43 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKuPBb2U4Fvm1QxIGTstjguI0rRXYJx5dTRA&usqp=CAU"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Aashay" name="name" />
+          <label>Email</label>
+          <input type="email" placeholder="aashay@gmail.com" name="email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
